c1fab9c34588ce56a889b7735d1f6bcff2f38416
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2024-05-04 11:37:07 +0000

    fix patching

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,13 @@ def mock_hardware_modules():
     sys.modules["utime"] = MagicMock()
     sys.modules["ntptime"] = MagicMock()
 
+    # Mock the Stepper class from the uln2003 module
+    stepper_mock = MagicMock()
+    uln2003 = MagicMock()
+    uln2003.Stepper.return_value = stepper_mock
+
+    sys.modules["uln2003"] = uln2003
+
     # Optional: Cleanup after the test session is done
     yield  # This allows the test session to run with the mocks in place
 
@@ -22,3 +29,4 @@ def mock_hardware_modules():
     del sys.modules["machine"]
     del sys.modules["utime"]
     del sys.modules["ntptime"]
+    del sys.modules["uln2003"]
