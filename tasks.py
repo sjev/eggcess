ee20aebbdb1808704103a694c6410afe64c784ec
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2024-05-06 21:10:20 +0000

    update automation

--- tasks.py
+++ tasks.py
@@ -34,50 +34,54 @@ def read_syncignore():
     return []
 
 
+def upload_file(base_url, auth, local_path, dest):
+    """Upload a single file."""
+
+    with open(local_path, "rb") as file:
+        # Read the whole file content and send as raw binary data
+        file_content = file.read()
+        headers = {
+            "Content-Type": "application/octet-stream"
+        }  # Set the content-type if necessary
+        response = requests.put(
+            f"{base_url}{dest}", data=file_content, auth=auth, headers=headers
+        )
+        print(f"Uploading {local_path} to {dest}: {response.status_code}")
+
+
 @task
-def push(ctx, src="src/", dest="/"):
+def push(ctx, filename=None, src="src/", dest="/"):
     """
-    Push files from the local 'src' directory to the device, ignoring hidden files and .syncignore patterns.
+    Push files from the local 'src' directory or a specific file to the device, ignoring hidden files and .syncignore patterns.
     """
     secrets = get_secrets()
     base_url = f"http://{secrets['DEVICE_IP']}/fs/"
     auth = ("", secrets["DEVICE_PASS"])
     ignore_patterns = read_syncignore()
 
-    for root, dirs, files in os.walk(src):
-        files = [
-            f
-            for f in files
-            if not f.startswith(".")
-            and all(not fnmatch(f, pat) for pat in ignore_patterns)
-        ]
-        for filename in files:
-            local_path = os.path.join(root, filename)
-            relative_path = os.path.relpath(local_path, src)
-            device_path = f"{dest}{relative_path}".replace(
-                "\\", "/"
-            )  # Ensure correct path separators
-            with open(local_path, "rb") as file:
-                # Read the whole file content and send as raw binary data
-                file_content = file.read()
-                headers = {
-                    "Content-Type": "application/octet-stream"
-                }  # Set the content-type if necessary
-                response = requests.put(
-                    f"{base_url}{device_path}",
-                    data=file_content,
-                    auth=auth,
-                    headers=headers,
-                )
-                print(
-                    f"Uploading {local_path} to {device_path}: {response.status_code}"
-                )
+    if filename:
+        file_path = Path(src) / filename
+        if file_path.exists():
+            upload_file(base_url, auth, file_path, dest)
+        else:
+            print(f"File {filename} does not exist")
+    else:
+        for root, dirs, files in os.walk(src):
+            files = [
+                f
+                for f in files
+                if not f.startswith(".")
+                and all(not fnmatch(f, pat) for pat in ignore_patterns)
+            ]
+            for filename in files:
+                local_path = Path(root) / filename
+                upload_file(base_url, auth, local_path, dest)
 
 
 @task
 def pull(ctx, dest="src/", src="/"):
     """
-    Pull files from the device to the local 'dest' directory, ignoring hidden files and .syncignore patterns.
+    Pull files from the device to the local 'dest' directory , ignoring hidden files and .syncignore patterns.
     """
     secrets = get_secrets()
     base_url = f"http://{secrets['DEVICE_IP']}/fs/"
@@ -106,34 +110,3 @@ def pull(ctx, dest="src/", src="/"):
                         with open(local_path, "wb") as file:
                             file.write(response.content)
                         print(f"Downloaded {device_path} to {local_path}")
-
-
-@task
-def upload_lut(ctx):
-    """
-    Upload the 'sun_lut.csv' file using the CircuitPython web workflow API.
-    """
-    secrets = get_secrets()
-    base_url = f"http://{secrets['DEVICE_IP']}/fs/"
-    auth = ("", secrets["DEVICE_PASS"])
-    file_path = Path("calculations/sun_lut.csv")
-
-    # Check if file exists
-    assert file_path.exists(), f"File {file_path} does not exist"
-    print(f"Uploading {file_path}...")
-
-    # Open the file and send it to the device
-    with file_path.open("rb") as file:
-        files = {"file": ("sun_lut.csv", file, "text/csv")}
-        device_path = "lib/sun_lut.csv"  # Specify the destination path on the device
-        response = requests.put(f"{base_url}{device_path}", files=files, auth=auth)
-
-        # Check response status
-        if response.status_code in [200, 201]:
-            print("File uploaded successfully.")
-        else:
-            raise Exception(
-                f"Failed to upload file: {response.status_code} {response.reason}"
-            )
-
-    print("Done")
