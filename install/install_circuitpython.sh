becd97450622bc7fb1a8154c569139da0c069463
Jev <jev.kuznetsov@gmail.com>
2025-02-09 17:18:19 +0100

    update flash script

--- install/install_circuitpython.sh
+++ install/install_circuitpython.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-FW_FILE=adafruit-circuitpython-seeed_xiao_esp32c3-en_US-9.0.4.bin
+PORT=/dev/ttyACM0
+FW_FILE=adafruit-circuitpython-seeed_xiao_esp32c3-en_US-9.2.4.bin
+
+# erase
+esptool.py --chip esp32c3 --port $PORT erase_flash
 
 
 # program
-esptool.py --chip esp32c3 --port /dev/ttyACM0 --baud 921600 --before default_reset \
-            --after hard_reset --no-stub  write_flash --flash_mode dio \
-            --flash_freq 80m 0x0 $FW_FILE
+esptool.py --chip esp32c3 --port $PORT --baud 921600 write_flash -z 0x0 $FW_FILE
