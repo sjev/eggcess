46f58a5f1314f22eeb7343429a1df45367ca59b6
Jev <jev.kuznetsov@gmail.com>
2025-02-17 10:00:29 +0100

    extend tasks

--- tasks.py
+++ tasks.py
@@ -105,3 +105,33 @@ def pull(ctx, dest="src/", src="/"):
                         with open(local_path, "wb") as file:
                             file.write(response.content)
                         print(f"Downloaded {device_path} to {local_path}")
+
+
+@task
+def upload_src(ctx):
+    """upload all source files"""
+
+    # upload files from src
+    files = Path("src").glob("*.py")
+    for file in files:
+        # skip .example files
+        if "example" in file.name:
+            print(f"Skipping {file}")
+            continue
+
+        print(f"Uploading {file}...")
+        ctx.run(f"ampy put {file}")
+        print(f"Uploaded {file}")
+
+    print("Done")
+
+
+@task
+def upload_lut(ctx):
+    """upload the sun_lut.csv file"""
+    f = Path("calculations/sun_lut.csv")
+    assert f.exists(), f"File {f} does not exist"
+    print(f"Uploading {f}...")
+    ctx.run(f"ampy put {f}")
+
+    print("Done")
