e6b80064694e2345cb03f75024f03311d0a9cc8e
Jev <jev.kuznetsov@gmail.com>
2025-03-24 09:32:10 +0000

    update install script to download firmware

--- install/install_circuitpython.sh
+++ install/install_circuitpython.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+FW_VERSION="9.2.6"
+
+FW_URL=https://downloads.circuitpython.org/bin/seeed_xiao_esp32c3/en_US/adafruit-circuitpython-seeed_xiao_esp32c3-en_US-$FW_VERSION.bin
+
 PORT=/dev/ttyACM0
-FW_FILE=adafruit-circuitpython-seeed_xiao_esp32c3-en_US-9.2.4.bin
+
+# download
+FW_FILE=$(basename $FW_URL)
+if [ ! -f $FW_FILE ]; then
+    echo "Downloading $FW_FILE"
+    wget $FW_URL
+fi
 
 # erase
 esptool.py --chip esp32c3 --port $PORT erase_flash
-
 
 # program
 esptool.py --chip esp32c3 --port $PORT --baud 921600 write_flash -z 0x0 $FW_FILE
